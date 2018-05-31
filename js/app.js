df69e06d86e16f8cc10d8f0e87a7bf76953d4fa5
Sandra Czerkas <sandra.czerkas@gmail.com>
2018-05-30 20:42:05 -0400

    Cats

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 /*The number of clicks should increment when the cat picture is clicked.*/
-var elem =document.getElementById('cat');
-var score = document.getElementById('score');
+var elem =document.getElementById('cat_1');
+var score = document.getElementById('score_1');
+var elem =document.getElementById('cat_2');
+var score = document.getElementById('score_2');
 
 /* parseInt(); */
-$('#cat').click(function(e){
-   var presentScore = $('#score').text();
-   $('#score').text(parseInt(presentScore)+1);
+$('#cat_1').click(function(e){
+   var presentScore = $('#score_1').text();
+   $('#score_1').text(parseInt(presentScore)+1);
+});
+
+$('#cat_2').click(function(e){
+   var presentScore = $('#score_2').text();
+   $('#score_2').text(parseInt(presentScore)+1);
 });
